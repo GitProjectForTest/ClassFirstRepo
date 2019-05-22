4ddb8a899f874ac4a2127eb68447b51781eab129
Mukesh <mukeshmsrit2008@gmail.com>
2019-05-22 03:11:00 +0000

    Added third parameter to multiply function

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ def subtract(x,y):
     return x - y
 def multiply(x,y):
     return x * y
-def divide(x,y):
-    return x * y
+def divide(x,y,z):
+    return x * y *z
 def square(x,y):
     return (x*x + y*y)
